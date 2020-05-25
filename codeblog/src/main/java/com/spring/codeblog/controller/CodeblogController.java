d4f0484a3b8a8bdddce66f8f7e5b475e2f688a13
rayssasandrade <rayssa.andrade071@outlook.com>
2020-05-25 14:42:13 -0300

    Criando e salvando novos posts

--- codeblog/src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ codeblog/src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -4,11 +4,15 @@ import com.spring.codeblog.model.Post;
 import com.spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -32,5 +36,23 @@ public class CodeblogController {
         mv.addObject("post", post);
         return mv;
     }
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.GET)
+    public String getPostForm(){
+         return "postForm";
+    }
+
+    @RequestMapping(value = "/newpost", method = RequestMethod.POST)
+    public String savePost(@Validated Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            return "redirect:/newPost";
+        }
+        post.setData(LocalDate.now());
+        codeblogService.save(post);
+        return "redirect:/posts";
+    }
+
+
+
 
 }
