2fb2b30c8da1d8d48c0fea9cc970a01ceadb70e7
rayssasandrade <rayssa.andrade071@outlook.com>
2020-09-29 14:54:27 -0300

    trocando a porta e atualizando projeto

--- codeblog/src/main/java/com/spring/codeblog/utils/DummyData.java
+++ codeblog/src/main/java/com/spring/codeblog/utils/DummyData.java
@@ -27,7 +27,7 @@ public class DummyData {
         post1.setTexto("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
 
         Post post2 = new Post();
-        post2.setAutor("Michelli Brito");
+        post2.setAutor("Rayssa Andrade");
         post2.setData(LocalDate.now());
         post2.setTitulo("API REST");
         post2.setTexto("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
